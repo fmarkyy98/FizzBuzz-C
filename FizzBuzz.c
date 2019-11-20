6498c5da8ddf415a38c9cfa83ca3bac2c627f45d
Mark Black <fmark98@gmail.com>
2019-11-20 18:20:50 +0100

    Update FizzBuzz.c
    pretifyed.

--- FizzBuzz.c
+++ FizzBuzz.c
@@ -2,13 +2,13 @@
 
 int main(){
     for(int i = 1; i<20;++i) {
-        if (i%3==0 && i%5==0) {
+        if (i % 3 == 0 && i % 5 == 0) {
           printf("fizz, buzz\n");
         }
-        else if(i%3==0) {
+        else if(i % 3 == 0) {
             printf("fizz\n");
         }
-        else if(i%5==0){
+        else if(i % 5 == 0){
             printf("buzz\n");
         }
         else{
